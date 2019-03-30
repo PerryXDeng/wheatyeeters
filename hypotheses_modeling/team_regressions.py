b77dc26a2d55e2258da171ed7afac87d59af2064
PerryXDeng <PerryXDeng@users.noreply.github.com>
2019-03-30 17:17:55 -0400

    implemented linear regression with correlation and error metrics

--- hypotheses_modeling/team_regressions.py
+++ hypotheses_modeling/team_regressions.py
@@ -1,4 +1,32 @@
 from sklearn import linear_model
+import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score
 
-reg = linear_model.LinearRegression()
-reg
+
+def k_days_into_future_regression(X, y, k, n0):
+  col = "TimeSinceAugFirst"
+  inp = []
+  out = []
+  for day in y[col][n0 - 1:]:
+    prev = day - k
+    xprev = X[X[col] == prev].drop(columns=[col]).to_numpy()[0, :]
+    yt = y[y[col] == day].drop(columns=[col]).to_numpy()[0, :]
+    inp.append(xprev)
+    out.append(yt)
+  regr = linear_model.LinearRegression()
+  regr.fit(inp, out)
+  predictions = regr.predict(inp)
+  mse = mean_squared_error(out, predictions)/(len(out) - 2)
+  r2 = r2_score(out, predictions)
+  return regr.intercept_, regr.coef_, r2, mse
+
+
+def main():
+  fatigueSums = pd.read_csv("fatigue_total_sum.csv")
+  workMovingAverage21 = pd.read_csv("21DaySlidingWorkAverage.csv", index_col=0)
+  performance = pd.read_csv("time_series_days_ranked.csv", index_col=0)
+  print(k_days_into_future_regression(workMovingAverage21, fatigueSums, 0, 21))
+
+
+if __name__ == "__main__":
+  main()
