a0d704e1c9ff590e96d55ebb38a2f3eb11cbd41b
Ryan Missel <rxm7244@rit.edu>
2019-03-29 22:15:20 -0400

    Add dirty_wellness_na to fill in NaN values

--- data_preparation/vectorization_ex.py
+++ data_preparation/vectorization_ex.py
@@ -1,22 +1,12 @@
 import pandas as pd
 
 # read in CSV
-df = pd.read_csv('cleaned/dirty_wellness.csv')
+df = pd.read_csv('cleaned/dirty_wellness_na.csv')
 
 
 def vectorize_mult(column, dictionary, file=None):
-    """
-    Handles vectorizing
-    :param column:
-    :param dictionary:
-    :param file:
-    :return:
-    """
     newCol = column + "Num"
     df[newCol] = df[column].map(dictionary)
     if file is not None:
         df.to_csv('cleaned/{}.csv'.format(file))
 
-
-
-vectorize_mult("USGMeasurement", {"No": 0, "Yes": 1}, "wellness")
